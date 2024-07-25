f06ecda91ae3e7cca885f4b12873d9b2ab677196
Tumenjargal Altanginj <tumenjargal@pink.kiwi>
2024-07-24 21:20:13 -0500

    assignment 3

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const games = require('./files/games.json')
+const students = require('./files/school.json')
 
 const app = express();
 const port = 8484;
@@ -27,20 +29,45 @@ app.get('/page2.html', async (req, res) => {
 });
 
 app.get('/', async (req, res) => {
-    const data = await readStaticFiles('./templates/index.html');
-    res.setHeader('Content-Type', 'text/html');
-    res.send(data);
+    if (req.query.a !== undefined && req.query.b !== undefined) {
+        const result = parseInt(req.query.a) * parseFloat(req.query.b)
+        console.log(parseInt(req.query.a) * parseFloat(req.query.b))
+        res.setHeader('Content-Type', 'text/json');
+        res.send(JSON.stringify(result));
+    } else {
+        res.setHeader('Content-Type', 'text/json');
+        res.send(games);
+    }
 });
+
+app.get('/:a/:b', async (req, res) => {
+    const result = parseInt(req.params.a, 10) * parseFloat(req.params.b, 10)
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ result: result }));
+});
+
+app.get('/students/:num', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    console.log("hell")
+    const index = parseInt(req.params.num, 10);
+    if (index >= 0 && index < students.length) {
+        res.setHeader('Content-Type', 'application/json');
+        res.json(students[index]);
+    } else {
+        res.status(404).send('Student not found');
+    }
+});
+
+app.get('/students', (req, res) => {
+    res.setHeader('Content-Type', 'text/json')
+    res.send(JSON.stringify(students))
+})
+
 
 app.get('/index.html', async (req, res) => {
     const data = await readStaticFiles('./templates/index.html');
     res.setHeader('Content-Type', 'text/html');
     res.send(data);
-});
-
-app.get('/api/status', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.json({ status: 200, message: "Successful" });
 });
 
 app.use((req, res) => {
